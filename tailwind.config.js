320fd4abda3f7d40b77edafb3601e07e0867bf07
Alfian Faiz <alfian@smkn1kandeman.sch.id>
2022-01-25 14:34:47 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height: {
+        '128': '45rem',
+      },
+      width: {
+        '200': '64rem'
+      }
+    },
+  },
+  plugins: [
+
+  ],
+}
